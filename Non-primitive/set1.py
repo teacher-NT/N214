ecb9883a090cb45ea69bc944b7cd56dee62a2102
Teacher NT <alpholmon@gmail.com>
2026-07-28 15:24:20 +0500

    Set 1

--- Non-primitive/set1.py
+++ Non-primitive/set1.py
@@ -40,4 +40,33 @@ lst = ['Arab', 'Koreys', 'Nemis', 'Ingliz', 'Rus']
 # set4 = set3.union(lst)
 # print(set4)
 
-print(set3)
+# print(set3)
+
+# ============================================================
+
+set1 = {1,2,3,4,5,6}
+set2 = {4,5,6,7,8,9}
+
+# set3 = set1.intersection(set2)
+# print(set3)
+# print(set1)
+
+# set1.intersection_update(set2)
+# print(set1)
+
+# set3 = set1.difference(set2)
+# print(set3)
+
+# set1.difference_update(set2)
+# print(set1)
+
+# set3 = set1.symmetric_difference(set2)
+# print(set3)
+
+# set1.symmetric_difference_update(set2)
+# print(set1)
+
+set1 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14}
+set2 = {3,4,5,6}
+print(set2.issubset(set1))
+print(set1.issuperset(set2))
