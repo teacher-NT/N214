4add42c0b900917d9d09d9d89589ce5db589e961
Teacher NT <alpholmon@gmail.com>
2026-07-28 14:49:42 +0500

    Set 1

---
+++ Non-primitive/set1.py
@@ -0,0 +1,16 @@
+set1 = {34,5,12,21,43,52,48,12,5,34,12}
+# set1[3] = 6
+# print(set1[3])
+# print(set1)
+
+
+set2 = {"Apple", 'Peach', 'Cherry', 'Mango'}
+# if "Apple" in set2:
+#     print("Yes")
+# else:
+#     print("No")
+
+for i in set2:
+    print(i)
+
+print(len(set2))
