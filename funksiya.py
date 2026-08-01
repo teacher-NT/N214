97b810800ec69ccbf6729903a8a6c3a502d3af42
Teacher NT <alpholmon@gmail.com>
2026-08-01 14:22:50 +0500

    function

---
+++ funksiya.py
@@ -0,0 +1,11 @@
+
+def say_hello(ism, familya=""):
+    print(f"Salom {ism} {familya}")
+
+def func():
+    pass
+
+say_hello("Sevinch")
+say_hello("Sanjar", "Muhammadiyev")
+say_hello("Muhammadolim", "Abidov")
+say_hello(4, 5)
