5b47e2c3ad13e345cbb694ca19cf7c4a603eb3ee
Teacher NT <alpholmon@gmail.com>
2026-08-08 16:19:12 +0500

    json

--- Fayl/3.py
+++ Fayl/3.py
@@ -1,4 +1,24 @@
 import os
 os.system("cls")
 
-f = open("myfile.txt")
+import json
+# students = [
+#     {
+#         "Ism": "Bobur",
+#         "Yosh": 14,
+#         "Manzil": "Farg'ona"
+#     }, 
+#     {
+#         "Ism": "Sardor",
+#         "Yosh": 19,
+#         "Manzil": "Andijon"
+#     },
+# ]
+
+# with open("myfile.json", "w") as file:
+#     # file.writelines(students)
+#     json.dump(students, file, indent=4)
+
+with open("myfile.json") as file:
+    data = json.load(file)
+print(data)
