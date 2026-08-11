d168c5701e2d056062c48b5b5274116bc222b120
Teacher NT <alpholmon@gmail.com>
2026-08-11 14:57:30 +0500

    copy file

--- Fayl/4.py
+++ Fayl/4.py
@@ -1,9 +1,13 @@
 import os
 os.system("cls")
 
-with open("panda.jpeg", "rb") as file:
+with open("./N214/Fayl/panda.jpeg", "rb") as file:
     baytlar = file.read()
-    print(len(list(baytlar)))
-    
+    # print(len(list(baytlar)))
+
     # for i in list(baytlar):
     #     print(i, end=" ")
+
+with open("./N214/Fayl/kunfupanda.jpeg", "wb") as file:
+    file.write(baytlar)
+    print("Faylga yozildi")
