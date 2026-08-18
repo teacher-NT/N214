d5afce9f0527a6c6cead1073f75d0a6898b4ff7d
Teacher NT <alpholmon@gmail.com>
2026-08-18 14:36:57 +0500

    attribute

---
+++ OOP/2.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls")
+
+class Car:
+    brand = "BMW"
+    model = "E69"
+    price = 120000
+    color = "black"
+    year = 2015
+
+car1 = Car()
+car1.brand = "bmw"
+
+print(car1.brand)
