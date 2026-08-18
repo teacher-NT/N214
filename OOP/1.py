4fd217637d25a56816d40224d0e87a4f4d5add81
Teacher NT <alpholmon@gmail.com>
2026-08-18 14:28:07 +0500

    OOP

---
+++ OOP/1.py
@@ -0,0 +1,18 @@
+import os
+os.system("cls")
+
+class Car:
+    pass
+
+car1 = Car()
+
+a = 14
+b = 4.5
+c = "Salom"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(car1))
