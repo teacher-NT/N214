851573863fd68713e73f729020baf6a32618eecf
Teacher NT <alpholmon@gmail.com>
2026-08-22 15:03:34 +0500

    prolimorfizm

--- OOP/10.py
+++ OOP/10.py
@@ -9,8 +9,14 @@ class Dog:
     def sound(self):
         print("Dog is barking")
 
+class Cat(Animal):
+    def sound(self):
+        print("Cat is miov")
+
 a1 = Animal()
 d1 = Dog()
+c1 = Cat()
 
 a1.sound()
 d1.sound()
+c1.sound()
