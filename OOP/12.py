e335b7ea535df1bd688aaac716738b1acb546090
Teacher NT <alpholmon@gmail.com>
2026-08-25 14:41:17 +0500

    getter/setter

--- OOP/12.py
+++ OOP/12.py
@@ -6,11 +6,30 @@ class BankAccount:
         self.name = n
         self.__balans = b
 
+    def get_balans(self, key):
+        if key == "qwerty":
+            print(self.__balans)
+        else:
+            print("Kalit xato")
+
+    def set_balans(self, new, key):
+        if key == "qwerty":
+            self.__balans = new
+        else:
+            print("Kalit xato")
 
 
 user1 = BankAccount("Avazbek", 2000)
 user1.name = "Avazbek Ismoilov"
 print(user1.name)
 
-print(user1.__balans)
+# user1.get_balans("qwerty")
+# user1.set_balans(5000, "qwerty")
+# user1.get_balans("qwerty")
+
+
+# user1.__balans = 8000
+# print(user1.__balans)
+
+# user1.get_balans("qwerty")
         
