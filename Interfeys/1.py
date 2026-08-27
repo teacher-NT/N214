110ea871f5486bd0f10f99b24a1ade020f6644ae
Teacher NT <alpholmon@gmail.com>
2026-08-27 15:16:10 +0500

    interfeys 3

--- Interfeys/1.py
+++ Interfeys/1.py
@@ -1,7 +1,10 @@
 import os
-os.system("cls")
+# os.system("cls")
 
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import (
+    QApplication, QWidget, QLabel,
+    QPushButton
+)
 from PyQt5.QtGui import QFont
 
 
@@ -18,6 +21,14 @@ matn1.setStyleSheet("font-size: 28px; color:blue; font-weight:bold;")
 matn1.setFont(QFont("Ink Free", 18))
 matn1.move(30, 20)
 
+def func_btn():
+    print("Tugmacha bosildi")
+
+tugma1 = QPushButton(oyna)
+tugma1.setText("Press Me")
+tugma1.setGeometry(100, 200, 200, 50)
+tugma1.setStyleSheet("font-size:24px; background-color: #ed8032; border: 2px solid black; border-radius:20px;")
+tugma1.clicked.connect(func_btn)
 
 oyna.show()
 app.exec_()
