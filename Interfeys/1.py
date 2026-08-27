36439a5f2c1b868b600d3af3642a603632133986
Teacher NT <alpholmon@gmail.com>
2026-08-27 14:51:00 +0500

    interfeys 2

--- Interfeys/1.py
+++ Interfeys/1.py
@@ -1,14 +1,24 @@
 import os
 os.system("cls")
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtGui import QFont
+
 
 app = QApplication([])
 
 oyna = QWidget()
 oyna.setWindowTitle("Dastur")
 oyna.setGeometry(1400, 100, 400, 700)
-oyna.show()
+oyna.setStyleSheet("background-color: #d9d1bd;")
 
+matn1 = QLabel(oyna)
+matn1.setText("Ilovamizga Xush kelibsiz")
+matn1.setStyleSheet("font-size: 28px; color:blue; font-weight:bold;")
+matn1.setFont(QFont("Ink Free", 18))
+matn1.move(30, 20)
+
+
+oyna.show()
 app.exec_()
 
